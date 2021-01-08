c0ec62c404e660aae186a544f8bd4256d9f66270
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-08 16:50:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,5 @@
-current_time = Time.now
-current_time = current_time.to_i
-if current_time.even?
-  puts "Even!"
-else
-  puts "Odd!"
+count = 0 # A bit of data defined outside the loop
+while count < 3 do # A Boolean expression using the bit of data
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1 # A bit of work that moves the bit of data closer to being false
 end
